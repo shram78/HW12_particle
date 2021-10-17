4340ce33a66bdfa92026e55ba77cadf4738330b3
shram78 <ra00275@gmail.com>
2021-10-17 21:24:34 +0300

    complete

---
+++ Assets/Scripts/Movable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class Movable : MonoBehaviour
+{
+    [SerializeField] ParticleSystem _particleSystem;
+
+    private Animator _animator;
+    private const string _buttonPressed = "isButtonPressed";
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _animator.SetBool(_buttonPressed, false);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _animator.SetBool(_buttonPressed, true);
+            _particleSystem.Play();
+        }
+    }
+}
